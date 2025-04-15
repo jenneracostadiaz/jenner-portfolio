107db78204acd1a810a12ee163b6f3f74f319e77
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2025-04-15 16:22:03 -0500

    chore(icons): add icons references

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 // src/components/icons/index.ts
-export { default as ArrowRight } from './Icons/ArrowRight.astro';
-export { default as Github } from './Icons/Github.astro';
-export { default as Linkedin } from './Icons/Linkedin.astro';
+export { default as Github } from './icons/Github.astro';
+export { default as Linkedin } from './icons/Linkedin.astro';
+export { default as Instagram } from './icons/Instagram.astro';
+export { default as LinkOut } from './icons/LinkOut.astro';
 // Export other icons as you add them
