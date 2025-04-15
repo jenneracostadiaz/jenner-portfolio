05885e870a27935d9323e2594cf7b6c9daa1c166
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2025-04-15 16:20:56 -0500

    feat(system): add inline stylesheets never

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
+    build: {
+        inlineStylesheets: 'never',
+    },
     vite: {
         plugins: [tailwindcss()],
     }
