f423cf454f581d3d6ba5aa660ade5e185d9a5d30
Jenner Acosta Diaz <dev@jenner.pe>
2025-04-15 04:13:57 -0500

    feat(system): add seo items

---
+++ src/src/consts.ts
@@ -0,0 +1,13 @@
+export const SITE_TITLE = 'Jenner';
+export const SITE_DESCRIPTION =
+	'Blog about software development and other stuff';
+
+export const SITE_URL = 'https://jenner.pe';
+export const SITE_IMAGE = '/seo/blog-seo.jpg';
+
+export const SITE_SOCIAL = {
+	github: 'https://github.com/jenneracostadiaz',
+	linkedin: 'https://linkedin.com/in/jenneracostadiaz',
+	instagram: 'https://instagram.com/cjenneracostadiaz',
+	youtube: 'https://youtube.com/@jenneracostadiaz',
+};
