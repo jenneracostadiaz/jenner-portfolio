3b0b1dd8ee937d040a435f3050e308f7788fbe33
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2025-04-15 15:48:13 -0500

    chore(icons): add linkedin and github srt

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,5 @@
 // src/components/icons/index.ts
 export { default as ArrowRight } from './Icons/ArrowRight.astro';
+export { default as Github } from './Icons/Github.astro';
+export { default as Linkedin } from './Icons/Linkedin.astro';
 // Export other icons as you add them
