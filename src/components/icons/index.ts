5303c35ba9fac7f5e43b28f488900c512cc07d4e
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2025-04-15 20:18:05 -0500

    refactor(icons): order links component icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,8 @@
-// src/components/icons/index.ts
+/** Arrows */
+export { default as LinkOut } from './icons/LinkOut.astro';
+
+/** Socials */
 export { default as Github } from './icons/Github.astro';
 export { default as Linkedin } from './icons/Linkedin.astro';
 export { default as Instagram } from './icons/Instagram.astro';
-export { default as LinkOut } from './icons/LinkOut.astro';
-// Export other icons as you add them
+export { default as Youtube } from './icons/Youtube.astro';
