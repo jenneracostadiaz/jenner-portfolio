9ad7cb9c355f9ed036313ed49949f8c20684628f
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2025-04-15 20:20:13 -0500

    feat(config): add site data contact

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+export const SITE_TITLE = 'Jenner';
+export const SITE_DESCRIPTION =
+	'Blog about software development and other stuff';
+export const SITE_ICON = '/icon.svg';
+
+export const SITE_URL = 'https://jenner.pe';
+export const SITE_IMAGE = '/seo/blog-seo.jpg';
+
+export const SITE_SOCIAL = {
+	github: 'https://github.com/jenneracostadiaz',
+	linkedin: 'https://linkedin.com/in/cjenneracostadiaz',
+	instagram: 'https://instagram.com/jenneracostadiaz',
+	youtube: 'https://youtube.com/@jenneracostadiaz',
+};
+
+export const SITE_DATA_CONTACT = {
+	email: 'projects@jenner.pe',
+	phone: '+51 967 037 995',
+	walink: 'https://wa.me/51967037995',
+	address: 'Lima, Perú',
+};
+
+
