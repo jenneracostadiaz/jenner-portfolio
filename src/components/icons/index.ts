a803b6bbf15a4d474f1bdfd8422dd79edf385256
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-05-13 22:54:17 -0500

    chore(system): remove icons vault

--- src/components/icons/index.ts
+++
@@ -1,8 +0,0 @@
-/** Arrows */
-export { default as LinkOut } from './icons/LinkOut.astro';
-
-/** Socials */
-export { default as Github } from './icons/Github.astro';
-export { default as Linkedin } from './icons/Linkedin.astro';
-export { default as Instagram } from './icons/Instagram.astro';
-export { default as Youtube } from './icons/Youtube.astro';
