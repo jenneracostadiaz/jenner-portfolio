5ce23967b2cb1199293bd4c5295db865b3ceb472
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-09-16 15:26:26 -0500

    feat(i18n): enhance translation utilities with path translation and route retrieval

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, showDefaultLang } from './ui';
+import { ui, defaultLang, showDefaultLang, routes } from './ui';
 
 export function getLangFromUrl(url: URL) {
     const [, lang] = url.pathname.split('/');
@@ -14,6 +14,56 @@ export function useTranslations(lang: keyof typeof ui) {
 
 export function useTranslatedPath(lang: keyof typeof ui) {
     return function translatePath(path: string, l: string = lang) {
-        return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
+        const pathName = path.replaceAll('/', '');
+        const hasTranslation =
+            defaultLang !== l &&
+            routes[l as keyof typeof routes] !== undefined &&
+            routes[l as keyof typeof routes][
+                pathName as keyof (typeof routes)[typeof defaultLang]
+            ] !== undefined;
+        const translatedPath = hasTranslation
+            ? '/' +
+              routes[l as keyof typeof routes][
+                  pathName as keyof (typeof routes)[typeof defaultLang]
+              ]
+            : path;
+
+        const normalizedPath = translatedPath.startsWith('/')
+            ? translatedPath
+            : `/${translatedPath}`;
+        const finalPath =
+            !showDefaultLang && l === defaultLang ? normalizedPath : `/${l}${normalizedPath}`;
+        return finalPath.replace(/\/+/g, '/');
     };
+}
+
+export function getRouteFromUrl(url: URL): string | undefined {
+    const pathname = new URL(url).pathname;
+    const parts = pathname?.split('/');
+    const path = parts.pop() || parts.pop();
+
+    if (path === undefined) {
+        return undefined;
+    }
+
+    const currentLang = getLangFromUrl(url);
+
+    if (defaultLang === currentLang) {
+        const route = Object.values(routes)[0];
+        return (route as Record<string, string>)[path] !== undefined
+            ? (route as Record<string, string>)[path]
+            : undefined;
+    }
+
+    const getKeyByValue = (obj: Record<string, string>, value: string): string | undefined => {
+        return Object.keys(obj).find((key) => obj[key] === value);
+    };
+
+    const reversedKey = getKeyByValue(routes[currentLang], path);
+
+    if (reversedKey !== undefined) {
+        return reversedKey;
+    }
+
+    return undefined;
 }
